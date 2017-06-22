f2b951455fe1fe4ebb62788004b161f27062f0c8
Nico Jansen <jansennico@gmail.com>
2017-06-23 00:03:40 +0200

    chore: Add post version script

--- tasks/post-version.ts
+++ tasks/post-version.ts
@@ -1,4 +1,10 @@
 import { execSync } from 'child_process';
 
-execSync('git push', { stdio: [0, 1, 2] });
-execSync('npm publish', { stdio: [0, 1, 2] });
+const execStreaming = (cmd: string) => {
+    console.log(`[release] ${cmd}`);
+    return execSync(cmd, { stdio: [0, 1, 2] });
+};
+
+execStreaming('git push');
+execStreaming('git push --tags');
+execStreaming('npm publish');
