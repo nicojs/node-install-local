617f54c4b283558c55818ba99a23785b8c92b409
Nico Jansen <jansennico@gmail.com>
2017-09-21 07:45:44 +0200

    chore(mutation testing): Add stryker mutation testing

---
+++ stryker.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+  config.set({
+    tsconfigFile: 'tsconfig.json',
+    files: [
+      '!test/integration/**/*.ts'
+    ],
+    mutate: [
+      'src/**/*.ts',
+      '!src/**/*.d.ts'
+    ],
+    mutator: 'typescript',
+    transpilers: [
+      'typescript'
+    ],
+    testRunner: "mocha",
+    reporter: ["html", "clear-text", "progress"],
+    testFramework: "mocha",
+    coverageAnalysis: "off",
+    thresholds: {
+      high: 90,
+      low: 85,
+      break: 85
+    }
+  });
+};
