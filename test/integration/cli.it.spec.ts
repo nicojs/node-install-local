ed125c0e4b67d12919c40171867f13d752d7c032
Nico Jansen <jansennico@gmail.com>
2025-10-21 12:12:43 +0200

    Fix ci on windows

--- test/integration/cli.it.spec.ts
+++ test/integration/cli.it.spec.ts
@@ -102,8 +102,8 @@ describe('install-local cli', () => {
     const expectedLink = await fs.readlink(
       path.resolve(packages.one.directory, 'node_modules', 'typed-inject'),
     );
-    expect(expectedLink).to.eq(
-      '.pnpm/typed-inject@5.0.0/node_modules/typed-inject',
+    expect(expectedLink).to.contain(
+      path.join('.pnpm', 'typed-inject@5.0.0', 'node_modules', 'typed-inject'),
     ); // verify arrange
     const expectedPnpmLock = await packages.one.readFile('pnpm-lock.yaml');
 
